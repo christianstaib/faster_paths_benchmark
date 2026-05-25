d36cb4e9387ab7b83c5d57d93dda73b3f0b3c45a
Christian Staib <staib.christian@hotmail.de>
2026-05-25 20:18:55 +0200

    added hl everything bin

---
+++ src/bin/hl_merge_validate_benchmark.rs
@@ -0,0 +1,76 @@
+use clap::Parser;
+use faster_paths::{
+    contraction_hierarchy::ContractionHierarchy,
+    hub_labeling::{HubLabeling, HubLabelingPathfinder},
+    validation::{validate_distances, validate_paths, PathTestCase},
+};
+use faster_paths_benchmarks::{
+    benchmark_pathfinder, load_graph_edges, report_validation_result, DistanceType,
+};
+use std::{fs::File, io::BufReader, path::PathBuf, time::Instant};
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Input graph file
+    #[arg(short, long)]
+    graph: PathBuf,
+
+    /// Contraction hierarchy file
+    #[arg(short, long)]
+    contraction_hierarchy: PathBuf,
+
+    /// Test file
+    #[arg(short, long)]
+    tests: PathBuf,
+
+    /// Absolute comparison tolerance
+    #[arg(short, long)]
+    epsilon: DistanceType,
+
+    /// Number of benchmark queries
+    #[arg(short, long)]
+    num: usize,
+}
+
+fn main() {
+    let args = Args::parse();
+
+    let (contraction_hierarchy, _): (ContractionHierarchy<DistanceType>, _) = postcard::from_io((
+        BufReader::new(File::open(&args.contraction_hierarchy).unwrap()),
+        &mut [0; 1024],
+    ))
+    .unwrap();
+
+    let start = Instant::now();
+    let hub_labeling =
+        HubLabeling::try_from_contraction_hierarchy(&contraction_hierarchy, args.epsilon).unwrap();
+    println!("Merging took {:?}", start.elapsed());
+
+    let avg_label_size = hub_labeling.up_hub_labeling().num_flat() as f32
+        / hub_labeling.up_hub_labeling().num_nested() as f32;
+    println!("Average label size is {}", avg_label_size);
+
+    let graph_edges = load_graph_edges(&args.graph);
+    let tests_input = File::open(&args.tests).unwrap();
+    let tests: Vec<PathTestCase<DistanceType>> =
+        serde_json::from_reader(BufReader::new(tests_input)).unwrap();
+
+    let mut pathfinder = HubLabelingPathfinder::new(&contraction_hierarchy, &hub_labeling);
+
+    let distance_result = validate_distances(&tests, &mut pathfinder, args.epsilon);
+    let path_result = validate_paths(&tests, &graph_edges, &mut pathfinder, args.epsilon);
+
+    let distances_valid = report_validation_result("distances", tests.len(), distance_result);
+    let paths_valid = report_validation_result("paths", tests.len(), path_result);
+
+    if !distances_valid || !paths_valid {
+        std::process::exit(1);
+    }
+
+    benchmark_pathfinder(
+        &mut pathfinder,
+        contraction_hierarchy.num_vertices(),
+        args.num,
+    );
+}
